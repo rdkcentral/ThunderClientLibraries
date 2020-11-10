8536fa84f4a9842789f8d17839a2e95ce8b694a5
Lukasz Iwan <l.iwan@metrological.com>
2020-11-10 03:39:41 -0800

    Implemented firmware_version and chipset functions + added necessary classes - not tested, not refactored

--- Source/deviceidentification/DeviceIdentification.cpp
+++ Source/deviceidentification/DeviceIdentification.cpp
@@ -19,15 +19,215 @@
 namespace WPEFramework {
 class DeviceIdentification : public Core::IReferenceCounted {
 private:
+    mutable int _refCount;
+    const string _name;
+    Exchange::IDeviceProperties* _deviceConnection;
+    static DisplayInfo::DisplayInfoAdministration _administration;
+
 #ifdef __WINDOWS__
 #pragma warning(disable : 4355)
 #endif
-    DisplayInfo(const string& displayName,
-        Exchange::IConnectionProperties* interface)
+    DeviceIdentification(const string& deviceName, Exchange::IDeviceProperties* interface)
+        : _refCount(1)
+        , _name(deviceName)
+        , _deviceConnection(interface)
     {
+        ASSERT(_deviceConnection != nullptr);
+        _deviceConnection->AddRef();
     }
 #ifdef __WINDOWS__
 #pragma warning(default : 4355)
 #endif
+    ~DeviceIdentification()
+    {
+        if (_deviceConnection != nullptr) {
+            _deviceConnection->Release();
+        }
+    }
+    class DeviceIdAdministration : protected std::list<DeviceIdentification*> {
+    public:
+        DeviceIdAdministration(const DeviceIdAdministration&) = delete;
+        DeviceIdAdministration& operator=(const DeviceIdAdministration&) = delete;
+
+        DeviceIdAdministration()
+            : _adminLock()
+            , _engine(Core::ProxyType<RPC::InvokeServerType<1, 0, 8>>::Create())
+            , _comChannel(Core::ProxyType<RPC::CommunicatorClient>::Create(Connector(), Core::ProxyType<Core::IIPCServer>(_engine)))
+        {
+            ASSERT(_engine != nullptr);
+            ASSERT(_comChannel != nullptr);
+            _engine->Announcements(_comChannel->Announcement());
+        }
+
+        ~DeviceIdAdministration()
+        {
+            std::list<DeviceIdentification*>::iterator index(std::list<DeviceIdentification*>::begin());
+
+            while (index != std::list<DeviceIdentification*>::end()) {
+                Trace("Closing %s", (*index)->Name().c_str());
+            }
+
+            ASSERT(std::list<DeviceIdentification*>::size() == 0);
+        }
+
+        DeviceIdentification* Instance(const string& name)
+        {
+            DeviceIdentification* result(nullptr);
+
+            _adminLock.Lock();
+
+            result = Find(name);
+
+            if (result == nullptr) {
+                Exchange::IDeviceProperties* interface = _comChannel->Open<Exchange::IDeviceProperties>(name);
+
+                if (interface != nullptr) {
+                    result = new DeviceIdentification(name, interface);
+                    std::list<DeviceIdentification*>::emplace_back(result);
+                    interface->Release();
+                }
+            }
+            _adminLock.Unlock();
+
+            return (result);
+        }
+
+        static Core::NodeId Connector()
+        {
+            const TCHAR* comPath = ::getenv(_T("COMMUNICATOR_PATH"));
+
+            if (comPath == nullptr) {
+#ifdef __WINDOWS__
+                comPath = _T("127.0.0.1:62000");
+#else
+                comPath = _T("/tmp/communicator");
+#endif
+            }
+
+            return Core::NodeId(comPath);
+        }
+
+        uint32_t Delete(const DeviceIdentification* deviceIdentification, int& refCount)
+        {
+            uint32_t result(Core::ERROR_NONE);
+
+            _adminLock.Lock();
+
+            if (Core::InterlockedDecrement(refCount) == 0) {
+                std::list<DeviceIdentification*>::iterator index(
+                    std::find(std::list<DeviceIdentification*>::begin(), std::list<DeviceIdentification*>::end(), deviceIdentification));
+
+                ASSERT(index != std::list<DeviceIdentification*>::end());
+
+                if (index != std::list<DeviceIdentification*>::end()) {
+                    std::list<DeviceIdentification*>::erase(index);
+                }
+                delete const_cast<DeviceIdentification*>(deviceIdentification);
+                result = Core::ERROR_DESTRUCTION_SUCCEEDED;
+            }
+
+            _adminLock.Unlock();
+
+            return result;
+        }
+
+    private:
+        DeviceIdentification* Find(const string& name)
+        {
+            DeviceIdentification* result(nullptr);
+
+            std::list<DeviceIdentification*>::iterator index(std::list<DeviceIdentification*>::begin());
+
+            while ((index != std::list<DeviceIdentification*>::end()) && ((*index)->Name() != name)) {
+                index++;
+            }
+
+            if (index != std::list<DeviceIdentification*>::end()) {
+                result = *index;
+                result->AddRef();
+            }
+
+            return result;
+        }
+
+        Core::CriticalSection _adminLock;
+        Core::ProxyType<RPC::InvokeServerType<1, 0, 8>> _engine;
+        Core::ProxyType<RPC::CommunicatorClient> _comChannel;
+    };
+
+public:
+    DeviceIdentification() = delete;
+    DeviceIdentification(const DeviceIdentification&) = delete;
+    DeviceIdentification& operator=(const DeviceIdentification&) = delete;
+
+    static DeviceIdentification* Instance(const string& name)
+    {
+        return _administration.Instance(name);
+    }
+    void AddRef() const
+    {
+        Core::InterlockedIncrement(_refCount);
+    }
+    uint32_t Release() const
+    {
+        return _administration.Delete(this, _refCount);
+    }
+
+    const string& Name() const { return _name; }
+
+    const string& Chipset() const
+    {
+        ASSERT(_deviceConnection != nullptr);
+        if (_deviceConnection != nullptr) {
+            return _deviceConnection->Chipset();
+        }
+        return string();
+    }
+
+    const string& FirmwareVersion() const
+    {
+        ASSERT(_deviceConnection != nullptr);
+        if (_deviceConnection != nullptr) {
+            return _deviceConnection->FirmwareVersion();
+        }
+        return string();
+    }
 };
+/* static */ DeviceIdentification::DisplayInfoAdministration DisplayInfo::_administration;
+
 } // namespace WPEFramework
+
+using namespace WPEFramework;
+extern "C" {
+
+struct deviceidentification_type* deviceidentification_instance(const char name[] = "DeviceIdentification")
+{
+    return reinterpret_cast<deviceidentification_type*>(DeviceIdentification::Instance(string(name)));
+}
+void deviceidentification_release(struct deviceidentification_type* instance)
+{
+    reinterpret_cast<DeviceIdentification*>(instance)->Release();
+}
+int16_t deviceidentification_chipset(struct deviceidentification_type* instance, char buffer[], const uint8_t length)
+{
+    string chipset = reinterpret_cast<DeviceIdentification*>(instance)->Chipset();
+    strncpy(buffer, chipset.c_str(), length);
+
+    if (length < chipset.length()) {
+        return -chipset.length();
+    }
+    return 0;
+}
+int16_t deviceidentification_firmware_version(struct deviceidentification_type* instance, char buffer[], const uint8_t length)
+{
+    string firmware = reinterpret_cast<DeviceIdentification*>(instance)->FirmwareVersion();
+    strncpy(buffer, firmware.c_str(), length);
+
+    if (length < firmware.length()) {
+        return -firmware.length();
+    }
+    return 0;
+}
+
+
+}
