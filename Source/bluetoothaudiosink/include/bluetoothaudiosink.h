c9bbd6e3ee5bf879713554ebedef1b942657f684
sebaszm <s.szczepaniak@metrological.com>
2021-10-18 18:03:33 +0200

    [bluetoothaudiosink] Introduce bluetoothaudiosink library and sample app

---
+++ Source/bluetoothaudiosink/include/bluetoothaudiosink.h
@@ -0,0 +1,84 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2021 Metrological
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef BLUETOOTHAUDIOSINK_H
+#define BLUETOOTHAUDIOSINK_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#undef EXTERNAL
+#define EXTERNAL __attribute__((visibility("default")))
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    uint32_t sample_rate;
+    uint16_t frame_rate;
+    uint8_t resolution;
+    uint8_t channels;
+} bluetoothaudiosink_format_t;
+
+typedef enum {
+    BLUETOOTHAUDIOSINK_STATE_UNKNOWN = 0,
+    BLUETOOTHAUDIOSINK_STATE_UNASSIGNED,
+    BLUETOOTHAUDIOSINK_STATE_DISCONNECTED,
+    BLUETOOTHAUDIOSINK_STATE_CONNECTED,
+    BLUETOOTHAUDIOSINK_STATE_CONNECTED_BAD_DEVICE,
+    BLUETOOTHAUDIOSINK_STATE_CONNECTED_RESTRICTED,
+    BLUETOOTHAUDIOSINK_STATE_READY,
+    BLUETOOTHAUDIOSINK_STATE_STREAMING
+} bluetoothaudiosink_state_t;
+
+typedef void *bluetoothaudiosink_player_t;
+
+typedef void (*bluetoothaudiosink_operational_state_update_cb)(const bool running, void *user_data);
+
+typedef void (*bluetoothaudiosink_state_update_cb)(void *user_data);
+
+
+EXTERNAL uint32_t bluetoothaudiosink_register_operational_state_update_callback(const bluetoothaudiosink_operational_state_update_cb callback, void *user_data);
+
+EXTERNAL uint32_t bluetoothaudiosink_unregister_operational_state_update_callback(const bluetoothaudiosink_operational_state_update_cb callback);
+
+EXTERNAL uint32_t bluetoothaudiosink_register_state_update_callback(const bluetoothaudiosink_state_update_cb callback, void *user_data);
+
+EXTERNAL uint32_t bluetoothaudiosink_unregister_state_update_callback(const bluetoothaudiosink_state_update_cb callback);
+
+EXTERNAL uint32_t bluetoothaudiosink_state(bluetoothaudiosink_state_t *state);
+
+EXTERNAL uint32_t bluetoothaudiosink_acquire(const char *connector, const bluetoothaudiosink_format_t *format, const uint16_t buffer_size_in_frames);
+
+EXTERNAL uint32_t bluetoothaudiosink_relinquish(void);
+
+EXTERNAL uint32_t bluetoothaudiosink_speed(const int8_t speed);
+
+EXTERNAL uint32_t bluetoothaudiosink_time(uint32_t *time_ms);
+
+EXTERNAL uint32_t bluetoothaudiosink_frame(const uint16_t length, const uint8_t data[], uint16_t *consumed);
+
+EXTERNAL uint32_t bluetoothaudiosink_dispose(void);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // BLUETOOTHAUDIOSINK_H
