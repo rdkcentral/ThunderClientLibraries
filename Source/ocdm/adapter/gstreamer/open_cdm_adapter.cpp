40622df99d952faaf76892f3314e0a998eea2878
Neeraj Deshpande <n.deshpande@metrological.com>
2023-01-31 14:08:32 +0530

    Modified gstreamer ocdm adapter for MediaProperties related fix.

--- Source/ocdm/adapter/gstreamer/open_cdm_adapter.cpp
+++ Source/ocdm/adapter/gstreamer/open_cdm_adapter.cpp
@@ -271,11 +271,11 @@ OpenCDMError opencdm_gstreamer_session_decrypt_buffer(struct OpenCDMSession* ses
 
             //Get Stream Properties from GstCaps
             MediaProperties *spPtr = nullptr;
+            MediaProperties streamProperties;
             if(caps != nullptr){
                 gchar *capsStr = gst_caps_to_string (caps);
                 if (capsStr != nullptr) {
                     WPEFramework::Plugin::CapsParser capsParser;
-                    MediaProperties streamProperties;
                     capsParser.Parse(reinterpret_cast<const uint8_t*>(capsStr), strlen(capsStr));
                     streamProperties.height = capsParser.GetHeight();
                     streamProperties.width = capsParser.GetWidth();
